486ad2b4b1e020d57669b9d0893862e91d044d37
nisha2018 <nishachakraborty1021994@gmail.com>
2023-06-05 17:13:08 +0530

    Getting the app ready for deployment

--- public/js/app.js
+++ public/js/app.js
@@ -42,7 +42,7 @@ weatherForm.addEventListener('submit', (event) => {
     messageOne.textContent = 'Loading.....';
 
 
-        fetch('http://localhost:3000/weather?address='+search.value).then((response) => {
+        fetch('/weather?address='+search.value).then((response) => {
         response.json().then((data) => {
             if(data.error){
                 //console.log(data.error)
--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const geoCode = require('./utils/geoCode')
 const forecast = require('./utils/forecast')
 
 const app = express();
-
+const port = process.env.PORT || 3000
 
 //Define path for express config
 const publicDirectory = path.join(__dirname,'../public')
@@ -123,6 +123,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Welcome to Express JS');
+app.listen(port, () => {
+    console.log('Node Js Application running on '+port);
 })
